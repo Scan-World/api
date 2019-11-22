7a4a71c07637b5f96fffd14e2e6791e2d4794a75
mathias <mathiasgabriel@live.fr>
2019-11-22 10:35:23 +0100

    ajout du service google map

--- app/serverAPI.py
+++ app/serverAPI.py
@@ -12,9 +12,13 @@ nlp = spacy.load("fr_core_news_md")
 
 import ast
 import numpy as np
+import pandas as pd
 import re
 import unidecode
 
+import googlemaps
+
+gmaps = googlemaps.Client(key='<key-google-map>')
 
 app                         = Flask(__name__)
 app.config['SECRET_KEY']    = 'secret!'
@@ -25,6 +29,30 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def cleaner(text):
     return re.sub("[^a-z ]+", "", unidecode.unidecode(text.lower()))
+
+def scan_world_places_query(tag, what):
+    """Get point of interest in specific cities.    Parameters
+    ----------
+    tag: str
+        A city name, an adress, for instance.
+    what: str
+        What do you want to query?    Returns
+    -------
+    result: dict
+    """
+    location = list(gmaps.geocode("%s, france" % tag)[0]['geometry']['location'].values())
+    if tag in ["", " "]:
+        query_result = gmaps.places(query=what,
+                                    language='fr')
+    else:
+        query_result = gmaps.places(query=what,
+                                    location=location,
+                                    language='fr')
+    result = pd.DataFrame(query_result['results']).sort_values('rating', ascending=False).head(5).reset_index(drop=True)
+
+    result['url'] = "https://www.google.com/maps/place/?q=place_id:" + result['reference']
+    result = result[['name', 'rating', 'url']].to_dict('records')
+    return result
 
 def scan_world_instant_similarity(tag):
     """Give some similarity scores.
@@ -60,6 +88,7 @@ def scan_world_instant_similarity(tag):
 @app.route('/')
 def hello():
     return 'Hello World!'
+
 @app.route('/posts/<tag>', methods=['GET'])
 def getPosts(tag):
 
@@ -81,6 +110,32 @@ def getPosts(tag):
 
     results = scan_world_instant_similarity(tag)
     return  jsonify(posts=results)
+
+@app.route('/posts_by_cat/<tag>/<cat>', methods=['GET'])
+def getPostsByCat(tag, cat):
+
+    """
+    This examples uses FlaskRESTful Resource
+    It works also with swag_from, schemas and spec_dict
+       ---
+       parameters:
+       - name: tag
+         in: path
+         type: string
+       - name: cat
+         in: path
+         type: string
+
+       responses:
+         200:
+           description: A single user item
+           schema:
+             message: string
+    """
+
+    results = scan_world_places_query(tag,cat)
+    print(results)
+    return  jsonify(data=results)
 
 
 if __name__ == '__main__':
